19934bbba79dee74e6cdf9ee0105a2eac0c7ab7d
efremskiy <efremovae.off@yandex.ru>
2019-03-09 12:55:57 +0300

    Homework 8

--- src/main/java/homeworks/hw8/entities/MetalsAndColorsData.java
+++ src/main/java/homeworks/hw8/entities/MetalsAndColorsData.java
@@ -6,12 +6,4 @@ public class MetalsAndColorsData {
     public String color;
     public String metals;
     public String[] vegetables;
-
-    public MetalsAndColorsData(int[] summary, String[] elements, String color, String metals, String[] vegetables) {
-        this.summary = summary;
-        this.elements = elements;
-        this.color = color;
-        this.metals = metals;
-        this.vegetables = vegetables;
-    }
 }
